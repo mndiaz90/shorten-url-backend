29e0288177ba275300c3d4a97d6416c9513bc81f
mndiaz90 <mayler900123@gmail.com>
2022-06-28 11:29:56 +0200

    Validate original_url

--- src/url/entity/url.entity.ts
+++ src/url/entity/url.entity.ts
@@ -1,3 +1,4 @@
+import { Matches } from 'class-validator';
 import {
   Column,
   Entity,
@@ -7,7 +8,6 @@ import {
   JoinColumn,
 } from 'typeorm';
 import { Clicks } from './clicks.entity';
-
 @Entity()
 export class Url {
   @PrimaryGeneratedColumn()
@@ -20,6 +20,9 @@ export class Url {
   short_url: string;
 
   @Column()
+  @Matches(
+    'https?://(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)',
+  )
   original_url: string;
 
   @OneToMany(() => Clicks, (click) => click.url, {
